a3885d66ac2820ad97914f21084f3c7d69c030c2
eastoncrafter <74274138+eastoncrafter@users.noreply.github.com>
2022-06-22 00:46:28 -0400

    dev stuff

--- send.py
+++ send.py
@@ -2,8 +2,9 @@ import serial
 import time
 import argparse
 
-parser = argparse.ArgumentParser(description="Send gcode files to a reprap running marlin")
+parser = argparse.ArgumentParser(description="Send gcode files to a reprap running marlin") #define the parser
 
 parser.add_argument('-p','--port',help='the port of the printer',required=True)
 parser.add_argument('-f','--file',help='location of the gcode file',required=True)
 args = parser.parse_args()
+print(args) #for testing purposes
