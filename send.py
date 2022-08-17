71c7e4d727cb77dd0af46e1a11d344d7bb5d8bb8
eastoncrafter <74274138+eastoncrafter@users.noreply.github.com>
2022-08-17 15:42:41 -0400

    lots of changes

--- send.py
+++ send.py
@@ -2,6 +2,7 @@ import serial
 import time
 import argparse
 import time #not sure if this is needed yet
+
 parser = argparse.ArgumentParser(description="Send gcode files to a reprap running marlin") #define the parser
 parser.add_argument('-p','--port',help='the port of the printer',required=True)
 parser.add_argument('-f','--file',help='location of the gcode file',required=True)
@@ -12,4 +13,3 @@ ser = serial.Serial(args.port, 115200)
 ser.write("\r\n\r\n") #wake the printer
 time.sleep(2) #wait for it
 ser.flushInput() #just a precaution
-
